7cb91a381347505596724f8af354615c7102e423
Muramori71 <dannyroh2500@gmail.com>
2023-06-15 19:07:58 -0700

    updated files

--- 02-Challenge copy/Develop/server.js
+++ 02-Challenge copy/Develop/server.js
@@ -4,17 +4,11 @@ const fs = require('fs')
 
 const path = require('path');
 
-const uuid = require('./helpers/uuid');
+const uuid = require('uuid');
 
 const app = express();
 
 const PORT = 3001;
-
-let savedNotes =  JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-
-let newNote = req.body;
-
-savedNotes.push(newNote);
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -25,23 +19,24 @@ app.get('/notes', (req, res) =>
 
 app.get('/api/notes', (req, res) => 
  fs.readFile('./db/db.json', 'utf-8', (err, data) => res.send(data))
-)
+);
 
 app.get('*', (req, res) => 
-  res.sendFile(path.join(_dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 
-)
+);
 
-app.post('/api/notes', (req, res) =>
- res.readFile('./db/db.json', 'utf-8',  ))
+app.post('/api/notes', (req, res) => {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let newNote = req.body;
+  let newId = 'Id'
+  savedNotes.push(newNote);
+
  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
  console.log("A new note has been saved: ", newNote);
  res.json(savedNotes);
+});
 
- app.post('/api/notes', (req, res) => {
-  req.body 
-  res.json(req.body);
- })
 
 
 app.listen(PORT, () =>
